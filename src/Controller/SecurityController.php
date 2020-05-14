1ea9aa38515687a1b4d0114b9fa28cb8335d8580
Natuu <natyouu@gmail.com>
2020-05-14 14:17:39 +0200

    Security

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -15,9 +15,6 @@ class SecurityController extends AbstractController
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
         if ($this->getUser()) {
-            if( !$this->getUser()->getRoles('ROLE_UD') ) {
-                return $this->redirectToRoute('login');
-            }
             return $this->redirectToRoute('default');
         }
 
