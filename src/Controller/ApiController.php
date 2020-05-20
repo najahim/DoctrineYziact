0af9b002b64b511bc06d07e8a67e14681ba266a4
Natuu <natyouu@gmail.com>
2020-05-20 14:33:53 +0200

    Bad token

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -36,94 +36,94 @@ class ApiController extends AbstractController
                 ],
                 JsonResponse::HTTP_CREATED
             );
-        }
+        } else {
+            $data = json_decode(
+                $request->getContent(),
+                true
+            );
 
-        $data = json_decode(
-            $request->getContent(),
-            true
-        );
+            if (isset($data['Nouveaux'])) {
+                foreach ($data['Nouveaux'] as $nouveau) {
+                    $mac = $nouveau['mac'];
+                    $rx = 0;
+                    $tx = 0;
 
-        if (isset($data['Nouveaux'])) {
-            foreach ($data['Nouveaux'] as $nouveau) {
-                $mac = $nouveau['mac'];
-                $rx = 0;
-                $tx = 0;
-
-                if (is_string ($mac) && preg_match('/([a-fA-F0-9]{2}:?){6}/', $mac) && is_numeric($rx) && is_numeric($tx)) {
-                    // nouvelle session avec $mac $rx $tx et l'heure actuelle en heure de début
-                    $session=new SessionWifi();
-                    $device=$this->getDoctrine()->getRepository('App:Peripherique')
-                        ->findBy(array('adresse_mac'=>$mac));
-                    // creer peripherique si n'existe pas
-                    if ($device == null)
-                    {
-                        $device=new Peripherique();
-                        $device->setAdresseMac($mac);
+                    if (is_string ($mac) && preg_match('/([a-fA-F0-9]{2}:?){6}/', $mac) && is_numeric($rx) && is_numeric($tx)) {
+                        // nouvelle session avec $mac $rx $tx et l'heure actuelle en heure de début
+                        $session=new SessionWifi();
+                        $device=$this->getDoctrine()->getRepository('App:Peripherique')
+                            ->findBy(array('adresse_mac'=>$mac));
+                        // creer peripherique si n'existe pas
+                        if ($device == null)
+                        {
+                            $device=new Peripherique();
+                            $device->setAdresseMac($mac);
+                            $entityManager = $this->getDoctrine()->getManager();
+                            $entityManager->persist($session);
+                            $entityManager->flush();
+                        }
+                        $session->setPeripherique($device);
+                        $session->setDateDebut(new \DateTime('now'));
+                        $session->setOctetRx($rx);
+                        $session->setOctetTx($tx);
                         $entityManager = $this->getDoctrine()->getManager();
                         $entityManager->persist($session);
                         $entityManager->flush();
                     }
-                    $session->setPeripherique($device);
-                    $session->setDateDebut(new \DateTime('now'));
-                    $session->setOctetRx($rx);
-                    $session->setOctetTx($tx);
-                    $entityManager = $this->getDoctrine()->getManager();
-                    $entityManager->persist($session);
-                    $entityManager->flush();
                 }
             }
-        }
 
-        if (isset($data['Deconnectes'])) {
-            foreach ($data['Deconnectes'] as $deconnectes) {
-                $mac = $deconnectes['mac'];
-                $rx = $deconnectes['rx'];
-                $tx = $deconnectes['tx'];
+            if (isset($data['Deconnectes'])) {
+                foreach ($data['Deconnectes'] as $deconnectes) {
+                    $mac = $deconnectes['mac'];
+                    $rx = $deconnectes['rx'];
+                    $tx = $deconnectes['tx'];
 
-                if (is_string ($mac) && preg_match('/([a-fA-F0-9]{2}:?){6}/', $mac) && is_numeric($rx) && is_numeric($tx)) {
-                    // fin de session pour la derniere session de $mac avec pour valeur $rx $tx et l'heure actuelle en heure de fin
-                    $session=new SessionWifi();
+                    if (is_string ($mac) && preg_match('/([a-fA-F0-9]{2}:?){6}/', $mac) && is_numeric($rx) && is_numeric($tx)) {
+                        // fin de session pour la derniere session de $mac avec pour valeur $rx $tx et l'heure actuelle en heure de fin
+                        $session=new SessionWifi();
 
-                    $sessions=$this->getDoctrine()->getRepository('App:SessionWifi')
-                        ->findLast($mac);
-                    $session=$sessions[0];
-                    $session->setDateFin(new \DateTime('now'));
-                    $session->setOctetRx($rx);
-                    $session->setOctetTx($tx);
-                    $entityManager = $this->getDoctrine()->getManager();
-                    $entityManager->persist($session);
-                    $entityManager->flush();
+                        $sessions=$this->getDoctrine()->getRepository('App:SessionWifi')
+                            ->findLast($mac);
+                        $session=$sessions[0];
+                        $session->setDateFin(new \DateTime('now'));
+                        $session->setOctetRx($rx);
+                        $session->setOctetTx($tx);
+                        $entityManager = $this->getDoctrine()->getManager();
+                        $entityManager->persist($session);
+                        $entityManager->flush();
+                    }
                 }
             }
-        }
 
-        if (isset($data['Update'])) {
-            foreach ($data['Update'] as $update) {
-                $mac = $update['mac'];
-                $rx = $update['rx'];
-                $tx = $update['tx'];
+            if (isset($data['Update'])) {
+                foreach ($data['Update'] as $update) {
+                    $mac = $update['mac'];
+                    $rx = $update['rx'];
+                    $tx = $update['tx'];
 
-                if (is_string ($mac) && preg_match('/([a-fA-F0-9]{2}:?){6}/', $mac) && is_numeric($rx) && is_numeric($tx)) {
-                    // actualiser pour la derniere session de $mac avec pour valeur $rx $tx
-                    $sessions=$this->getDoctrine()->getRepository('App:SessionWifi')
-                        ->findLast($mac);
-                    $session=$sessions[0];
-                    //$session->setDateFin(new \DateTime('now'));
-                    $session->setOctetRx($rx);
-                    $session->setOctetTx($tx);
-                    $entityManager = $this->getDoctrine()->getManager();
-                    $entityManager->persist($session);
-                    $entityManager->flush();
+                    if (is_string ($mac) && preg_match('/([a-fA-F0-9]{2}:?){6}/', $mac) && is_numeric($rx) && is_numeric($tx)) {
+                        // actualiser pour la derniere session de $mac avec pour valeur $rx $tx
+                        $sessions=$this->getDoctrine()->getRepository('App:SessionWifi')
+                            ->findLast($mac);
+                        $session=$sessions[0];
+                        //$session->setDateFin(new \DateTime('now'));
+                        $session->setOctetRx($rx);
+                        $session->setOctetTx($tx);
+                        $entityManager = $this->getDoctrine()->getManager();
+                        $entityManager->persist($session);
+                        $entityManager->flush();
+                    }
                 }
             }
-        }
 
-        return new JsonResponse(
-            [
-                'status' => 'ok',
-            ],
-            JsonResponse::HTTP_CREATED
-        );
+            return new JsonResponse(
+                [
+                    'status' => 'ok',
+                ],
+                JsonResponse::HTTP_CREATED
+            );
+        }
     }
 
     /**
@@ -141,9 +141,9 @@ class ApiController extends AbstractController
                 ],
                 JsonResponse::HTTP_CREATED
             );
+        } else {
+            return $this->render('api/QoS.twig');
         }
-
-        return $this->render('api/QoS.twig');
     }
 
 }
